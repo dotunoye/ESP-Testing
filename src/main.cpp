f702c289605f7a74cf6622bca89377f55f23ba95
dotunoye <dotunoye20@gmail.com>
2026-06-29 19:23:26 +0100

    Refactor main.cpp to implement distance measurement and state management for fire detection

--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,156 @@
 #include<Arduino.h>
 
-const int ledPin = 4;
-const int buzzerPin = 16;
-const int buttonPin = 17;
-const int firePin = 33;
+int echoPin = 4;
+int trigPin = 16;
+int whitePin = 17;
+int redPin = 5;
+int buzzerPin = 18;
+int buttonPin = 19;
 
-unsigned long previousLed = 0;
+float distance = 0;
+bool whiteState = HIGH;
+bool redState = HIGH;
+bool buzzerState = HIGH;
+
+unsigned long previousTrig = 0;
+unsigned long previousWhite = 0;
+unsigned long previousRed = 0;
 unsigned long previousBuzzer = 0;
-unsigned long previousFire = 0;
 unsigned long previousSerial = 0;
 
-unsigned long ledInterval = 0;
+unsigned long trigInterval = 1;
+unsigned long whiteInterval = 1000;
+unsigned long redInterval = 200;
 unsigned long buzzerInterval = 500;
-unsigned long fireInterval = 100;
-unsigned long serialInterval = 1000;
-
-int fireValue = 0;
-bool ledState = HIGH;
-
-void fireSensorRead (){
-    fireValue = analogRead(firePin);
-}
-
-void blinkLed (){
-    ledState = !ledState;
-    digitalWrite(ledPin, ledState);
-}
+unsigned long serialInterval = 500;
 
 enum State {
-    NORMAL,
-    ALARM,
-    SILENT
+    IDLE,
+    APPROACHING,
+    TOO_CLOSE
 };
 
-State currentState = NORMAL;
+State currentState = IDLE;
 
-void updateState (){
+void updateState(){
     switch (currentState)
     {
-    case NORMAL:
-        if (fireValue <= 2000)
+    case IDLE:
+        if (distance < 25)
         {
-            currentState = ALARM;
+            currentState = APPROACHING;
         }
-        break;
-    
-    case ALARM:
-        if (digitalRead(buttonPin) == LOW)
+        break;   
+    case APPROACHING:
+        if (distance < 10)
         {
-            currentState = SILENT;
-        }else if (fireValue > 2000)
+            currentState = TOO_CLOSE;
+        }else if (distance >= 25)
         {
-            currentState = NORMAL;
-        }
+            currentState = IDLE;
+            }
         
         break;
-
-    case SILENT:
-        if (fireValue > 2000)
+    case TOO_CLOSE:
+        if (distance >= 25)
         {
-            currentState = NORMAL;
-        }
-        break;
+            currentState = IDLE;
+        }else if (digitalRead(buttonPin) == HIGH)
+        {
+            currentState = IDLE;
+        }        
     }
-
-    }
+}
 
 void runState (){
     switch (currentState)
     {
-    case NORMAL:
+    case IDLE:
+        digitalWrite(whitePin, HIGH);
+        digitalWrite(redPin, LOW);
         digitalWrite(buzzerPin, LOW);
-        ledInterval = 1000;
+        whiteInterval = 0;
+        redInterval = 0;
+        buzzerInterval = 0;
         break;
     
-    case ALARM:
-        digitalWrite(buzzerPin, HIGH);
-        ledInterval = 200;
+    case APPROACHING:
+        whiteInterval = 500;
+        buzzerInterval = 1000;
+        digitalWrite(redPin, HIGH);
         break;
-
-    case SILENT:
-        digitalWrite(buzzerPin, LOW);
-        digitalWrite(ledPin, HIGH);
+    case TOO_CLOSE:
+        digitalWrite(whitePin, LOW);
+        redInterval = 250;
+        buzzerInterval = 200;
         break;
     }
+
+}
+
+void redLight (){
+    redState = !redState;
+    digitalWrite(redPin, redState);
+}
+void whiteLight (){
+    whiteState = !whiteState;
+    digitalWrite(whitePin, whiteState);
+}
+void buzzerUpdate (){
+    buzzerState = !buzzerState;
+    digitalWrite(buzzerPin, buzzerState);
 }
 
 void setup() {
     Serial.begin(115200);
-    pinMode(ledPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    pinMode(trigPin, OUTPUT);
+    pinMode(whitePin, OUTPUT);
+    pinMode(redPin, OUTPUT);
     pinMode(buzzerPin, OUTPUT);
     pinMode(buttonPin, INPUT_PULLUP);
-    pinMode(firePin, INPUT);
 }
 void loop() {
     unsigned long currentMillis = millis();
 
-    if (currentMillis - previousFire >= fireInterval)
-    {
-        previousFire = currentMillis;
-        fireSensorRead();
-    }
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    long duration = pulseIn(echoPin, HIGH);
+
+    distance = duration * 0.034/2;
 
     updateState();
-
     runState();
 
-    if (currentMillis - previousLed >= ledInterval)
+    if (currentMillis - previousWhite >= whiteInterval)
     {
-        previousLed = currentMillis;
-        blinkLed();
+        previousWhite = currentMillis;
+        whiteLight();
+    }
+
+    if (currentMillis - previousRed >= redInterval)
+    {
+        previousRed = currentMillis;
+        redLight();
+    }
+    
+    if (currentMillis - previousBuzzer >= buzzerInterval)
+    {
+        previousBuzzer = currentMillis;
+        buzzerUpdate();
     }
     
     if (currentMillis - previousSerial >= serialInterval)
     {
         previousSerial = currentMillis;
-        Serial.print("The value is:");
-        Serial.println(fireValue);
+        Serial.print("The distance is: ");
+        Serial.println(distance);
     }
     
+
+
 }
