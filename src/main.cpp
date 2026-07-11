ef9c0cf597cfbf1fce41e945974b73957a87643b
dotunoye <dotunoye20@gmail.com>
2026-07-11 13:58:37 +0100

    Refactor main.cpp to implement a state machine for LED control based on button presses and enhance edge detection for button inputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,103 @@
-#include<Arduino.h>
+#include <Arduino.h>
 
-int ledPin = 4;
-int buttonPin = 16;
-unsigned long interval = 500;
-unsigned long previousMillis = 0;
-bool ledState = false;
+int whiteLed = 4;
+int redLed = 16;
+int rgbLed1 = 17; /*Blue*/
+int rgbLed2 = 5;  /*Green*/
+int rgbLed3 = 18; /*Red*/
 
-enum State {
-    ON,
-    OFF
+int buttonWhite = 27;
+int buttonRed = 26;
+int buttonRgb = 25;
+
+bool redLedState = false;
+bool whiteLedState = false;
+
+unsigned long redMillis = 0;
+unsigned long whiteMillis = 0;
+unsigned long redInterval = 250;
+unsigned long whiteInterval = 700;
+unsigned long rgbMillis = 0;
+unsigned long rgbInterval = 500;
+
+
+enum redState {
+    R_ON,
+    R_OFF,
+    R_BLINK,
 };
 
-State currentState = OFF;
+redState redCurrent = R_OFF;
 
-void assignState (){
-    switch (currentState)
+bool lastRedState = HIGH;
+
+void assignRed (){
+    bool redRead = digitalRead(buttonRed);
+    bool redPressed = (redRead == LOW && lastRedState == HIGH);
+   if (redPressed) {
+        delay(50); // Wait out the physical bouncing
+        lastRedState = LOW; // Force the memory to sync cleanly with the press
+    } else {
+        lastRedState = redRead; // Otherwise track normally
+    }
+    switch (redCurrent)
     {
-    case ON:
-        if (digitalRead(buttonPin) == LOW)
+    case R_OFF:
+        if (redPressed)
         {
-            currentState = OFF;
+            redCurrent = R_ON;
         }
         break;
-    
-    case OFF:
-    if (digitalRead(buttonPin) == LOW)
-    {
-        currentState = ON;
-    }
-        break;
-    }
-}
-
-void executeState (unsigned long currentMilli){
-    switch (currentState)
-    {
-    case ON:
-        if (currentMilli - previousMillis >= interval)
+    case R_ON:
+        if (redPressed)
         {
-            previousMillis = currentMilli;
-            ledState = !ledState;
-            digitalWrite(ledPin, ledState);
-            delay(250);
-        }  
+            redCurrent = R_BLINK;
+        }
         break;
-    
-    case OFF:
-        digitalWrite(ledPin, LOW);
-        delay(250);
+    case R_BLINK:
+        if (redPressed)
+        {
+            redCurrent = R_OFF;
+        }
+    }
+}
+
+void executeRed(unsigned long currentMilli){
+    switch (redCurrent)
+    {
+    case R_OFF:
+        digitalWrite(redLed, LOW);
+        break;
+    case R_ON:
+        digitalWrite(redLed, HIGH);
+        break;
+    case R_BLINK:
+        if (currentMilli - redMillis >= redInterval)
+        {
+            redMillis = currentMilli;
+            redLedState = !redLedState;
+            digitalWrite(redLed, redLedState);
+        }
         break;
     }
 }
-void setup (){
-    pinMode(ledPin, OUTPUT);
-    pinMode(buttonPin, INPUT_PULLUP);
+void setup() {
+    Serial.begin(115200);
+    pinMode(whiteLed, OUTPUT);
+    pinMode(redLed, OUTPUT);
+    pinMode(rgbLed1, OUTPUT);
+    pinMode(rgbLed2, OUTPUT);
+    pinMode(rgbLed3, OUTPUT);
+    
+    pinMode(buttonRed, INPUT_PULLUP);
+    pinMode(buttonWhite, INPUT_PULLUP);
+    pinMode(buttonRgb, INPUT_PULLUP);
+
+    Serial.println("--- 9-State Advanced Master Controller Online ---");
 }
 
-void loop(){
+void loop() {
     unsigned long currentMillis = millis();
-    assignState();
-    executeState(currentMillis);
-    
+    assignRed();
+    executeRed(currentMillis);
 }
