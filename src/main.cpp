150d8f3b988e808134f0987a3d369b7c77a59dab
dotunoye <dotunoye20@gmail.com>
2026-07-17 18:22:47 +0100

    Refactor main.cpp to consolidate LED control logic and remove unused variables and button handling code using arrays

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,29 @@
 #include <Arduino.h>
 
-int whiteLed = 4;
-int redLed = 16;
-int rgbLed1 = 17; /*Blue*/
-int rgbLed2 = 5;  /*Green*/
-int rgbLed3 = 18; /*Red*/
+int ledPins[5] = {4, 16, 5, 18, 19};
+unsigned long previousMillis = 0;
+unsigned long interval = 300;
+int currentLed = 0;
 
-int buttonWhite = 27;
-int buttonRed = 26;
-int buttonRgb = 25;
-
-bool redLedState = false;
-bool whiteLedState = false;
-
-unsigned long redMillis = 0;
-unsigned long whiteMillis = 0;
-unsigned long redInterval = 250;
-unsigned long whiteInterval = 700;
-unsigned long rgbMillis = 0;
-unsigned long rgbInterval = 500;
-
-
-enum redState {
-    R_ON,
-    R_OFF,
-    R_BLINK,
-};
-
-redState redCurrent = R_OFF;
-
-bool lastRedState = HIGH;
-
-void assignRed (){
-    bool redRead = digitalRead(buttonRed);
-    bool redPressed = (redRead == LOW && lastRedState == HIGH);
-   if (redPressed) {
-        delay(50); // Wait out the physical bouncing
-        lastRedState = LOW; // Force the memory to sync cleanly with the press
-    } else {
-        lastRedState = redRead; // Otherwise track normally
-    }
-    switch (redCurrent)
-    {
-    case R_OFF:
-        if (redPressed)
-        {
-            redCurrent = R_ON;
-        }
-        break;
-    case R_ON:
-        if (redPressed)
-        {
-            redCurrent = R_BLINK;
-        }
-        break;
-    case R_BLINK:
-        if (redPressed)
-        {
-            redCurrent = R_OFF;
-        }
-    }
-}
-
-void executeRed(unsigned long currentMilli){
-    switch (redCurrent)
-    {
-    case R_OFF:
-        digitalWrite(redLed, LOW);
-        break;
-    case R_ON:
-        digitalWrite(redLed, HIGH);
-        break;
-    case R_BLINK:
-        if (currentMilli - redMillis >= redInterval)
-        {
-            redMillis = currentMilli;
-            redLedState = !redLedState;
-            digitalWrite(redLed, redLedState);
-        }
-        break;
-    }
-}
 void setup() {
     Serial.begin(115200);
-    pinMode(whiteLed, OUTPUT);
-    pinMode(redLed, OUTPUT);
-    pinMode(rgbLed1, OUTPUT);
-    pinMode(rgbLed2, OUTPUT);
-    pinMode(rgbLed3, OUTPUT);
-    
-    pinMode(buttonRed, INPUT_PULLUP);
-    pinMode(buttonWhite, INPUT_PULLUP);
-    pinMode(buttonRgb, INPUT_PULLUP);
 
-    Serial.println("--- 9-State Advanced Master Controller Online ---");
+    for (int i = 0; i < 5; i++)
+    {
+        pinMode(ledPins[i], OUTPUT);
+    }
 }
 
 void loop() {
     unsigned long currentMillis = millis();
-    assignRed();
-    executeRed(currentMillis);
+    if (currentMillis - previousMillis >= interval)
+        {
+            previousMillis = currentMillis;
+            for (int i = 0; i < 5; i++)
+            {
+                digitalWrite(ledPins[i], LOW);
+            }
+            digitalWrite(ledPins[currentLed], HIGH);
+           currentLed = (currentLed + 1) % 5;           
+        }
 }
