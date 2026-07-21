3a7a0b28cc30f1026b9b7775449d8e0969469b13
dotunoye <dotunoye20@gmail.com>
2026-07-21 23:36:53 +0100

    Battery sensor and reading with arrays

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,69 @@
 #include <Arduino.h>
 
-int sensorValues[] = {420, 650, 710, 600};
-int thresholds[]   = {300, 700, 760, 550};
-bool alarms[4];
-String state;
+float batteryVoltages[] = {12.4, 11.3, 12.9, 10.5, 11.9, 12.0};
+bool needReplacement [6];
+int batteryCount = 6;
+String status[6];
+int lowCount = 0;
+
 
 void setup() {
     Serial.begin(115200);
-    
-    for (int i = 0; i < 4; i++)
+    float lowestVolt = batteryVoltages[0];
+    int batteryIndex = 0;
+
+    for (int i = 0; i < batteryCount; i++)
     {
-        if (sensorValues[i] > thresholds[i])
+        if (batteryVoltages[i] < 11.5)
         {
-            alarms[i] =  true;
+            status[i] = "LOW";
+            lowCount ++;
         }else
         {
-            alarms[i] = false;
+            status[i] = "OK";
         }
-        if (alarms[i] == true)
-        {
-            state = "ALARM";
-        }else
-        {
-            state = "SAFE";
-        }
-        Serial.print("Sensor" );
+        Serial.print("Battery ");
         Serial.print(i);
-        Serial.print(": ");
-        Serial.println(state);
-    }  
+        Serial.print(" : ");
+        Serial.println(status[i]); 
+        
+    }
+    Serial.print("Total LOW Batteries: ");
+    Serial.println(lowCount);
+
+    for (int i = 1; i < batteryCount; i++)
+    {
+        if (batteryVoltages[i] < lowestVolt)
+        {
+            lowestVolt = batteryVoltages[i];
+            batteryIndex = i;
+        }
+    }
+        Serial.print("Lowest Voltage: ");
+        Serial.println(lowestVolt);
+        Serial.print("Battery Index: ");
+        Serial.println(batteryIndex); 
+    
+        for (int i = 0; i < batteryCount; i++)
+        {
+            needReplacement[i] = batteryVoltages[i] < 11.5;
+
+           if (needReplacement[i])
+           {                  
+            Serial.print("Battery ");
+            Serial.print(i);
+            Serial.print(" : ");
+            Serial.println("REPLACE");
+        }else
+        {
+            Serial.print("Battery ");
+            Serial.print(i);
+            Serial.print(" : ");
+            Serial.println("KEEP");
+        }
+    
+    }
 }
 void loop() {
+
 }
